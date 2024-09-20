4c65b524d6b2381c9e1907808717f7d753181dea
hugo.dominguez <hugo.dominguez@106-08.info2>
2024-09-20 13:49:20 +0200

    ejercicio dos

---
+++ src/Exercice2/Exercice2.java
@@ -0,0 +1,11 @@
+package Exercice2;
+
+public class Exercice2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, how are you?");
+		System.out.println("Fine thanks.");
+
+	}
+
+}
